50a9a0347e3ac5f1ad239043e01299e445e1d1bb
antoinecoulon <a.antoinecoulon@gmail.com>
2026-05-26 22:06:23 +0200

    feat: basic header component

---
+++ app/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+  <header class="h-16 bg-tertiary-900">
+    <div class="h-full flex justify-center items-center">
+      <h1 class="text-3xl text-secondary font-bold tracking-wide">
+        Westill
+      </h1>
+    </div>
+  </header>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,9 +4,7 @@
 
 
 <template>
-  <header class="h-16 bg-tertiary-900">
-    HEADER
-  </header>
+  <Header />
   <main class="h-screen bg-primary-900">
     <slot />
   </main>
